342f4c8cfc4a43ae6e12891296b52b2af240e462
drianks <benjamin.avendano04@inacapmail.cl>
2025-12-13 21:32:22 -0300

    Se hicieron unas correcciones

--- Ejercicio 2/Main.py
+++ Ejercicio 2/Main.py
@@ -5,8 +5,8 @@ from Empresa import Empresa
 
 def main():
     empresa = Empresa()
-    empresa.agregar(Vendedor("Lilian", "17.871.561-1", 450000, 1500000, 0.04))
-    empresa.agregar(Gerente("Benjamin", "22.472.652-2", 1200000, 200000))
+    empresa.agregar(Vendedor("Lilian", "17.871.561-1", 2330000, 12300000, 0.04))
+    empresa.agregar(Gerente("Benjamin", "22.472.652-2", 1420000, 4500000))
     empresa.agregar(Practicante("Edgard", "18.263.983-3", 60, 4500))
 
     print("\nTrabajadores activos:")
